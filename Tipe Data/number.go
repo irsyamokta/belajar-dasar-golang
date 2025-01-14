f26ae4d756e103948e29fcf29106e011fff38959
Irsyam Okta Pratama Riyadi <pratamalismee@gmail.com>
2025-01-14 14:46:25 +0700

    Belajar tipe data

---
+++ Tipe Data/number.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Satu = ", 1) //tipe data integer
+	fmt.Println("Dua = ", 2)
+	fmt.Println("Tiga koma Lima = ", 3.5) //tipe data float
+}
---
+++ Tipe Data/string.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Irsyam Okta")
+	fmt.Println(len("Irsyam")) //untuk menghitung panjang string
+	fmt.Println("Irsyam Okta Pratama Riyadi"[0]) //untuk mengambil karakter pertama
+}
